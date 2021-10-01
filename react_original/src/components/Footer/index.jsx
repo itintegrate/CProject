0a5a2f2e5394a1f7a1707e53ffe5c9efd8e1569b
ITintegrate <jin@itintgrate.com.au>
2021-10-01 17:17:21 +1000

    update

---
+++ react_original/src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Layout} from 'antd';
+const {Footer} = Layout;
+
+export default class PageFooter extends Component {
+    render() {
+        return (
+            <Footer>
+                footer......
+
+            </Footer>
+        )
+    }
+}
